9714db712650e19a3e1491ed07fdd2223d886fb1
xiaotongXT <xiaotong.wxt@qq.com>
2019-02-22 17:00:20 +0800

    首页完成1

--- src/javascript/control/app-index-control.js
+++ src/javascript/control/app-index-control.js
@@ -3,11 +3,13 @@ const appMain=require('../view/main.html')
 
 const rander=async()=>{
 	let list=await data();
+	let newlist=JSON.parse(list).data.list;
 	let template = Handlebars.compile(appMain);
-	$('#app .app-main-content').append(template({ films: JSON.parse(list).data.list }))
-	//console.log(JSON.parse(list).data.list);
-	
-	
+	$('#app .app-main-content').append(template({ films: newlist }));
+	$('#app .app-main-content').children().eq(0).find('.content-title img').attr('src','https://n1-q.mafengwo.net/s9/M00/FB/0C/wKgBs1diFRKAMMptAAAIpExA3RM367.png')
+
+	console.log(newlist);
 }
+
 
 module.exports={rander}
